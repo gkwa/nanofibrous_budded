4821ebc341546c03dc3a733fe2f974486a1741e4
Your Name <you@example.com>
2021-12-19 12:03:45 -0800

    t

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
-https://www.emqx.com/en/blog/how-to-use-mqtt-in-golang
-
+https://www.eclipse.org/paho/index.php?page=clients/golang/index.php
 https://hub.docker.com/_/golang
-
-docker pull golang
-docker run -ti golang bash
-
-```
-go version
-go version go1.13.12 darwin/amd64
-
-go get github.com/eclipse/paho.mqtt.golang
-```
-
-RUN go get github.com/eclipse/paho.mqtt.golang
-RUN mkdir /src
-COPY src/* /src
-
-WORKDIR /src
--- main.go
+++ main.go
@@ -1,36 +1,54 @@
 package main
 
 import (
-    "fmt"
-    mqtt "github.com/eclipse/paho.mqtt.golang"
-    "time"
+	"fmt"
+	//import the Paho Go MQTT library
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"os"
+	"time"
 )
 
-var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-    fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
-}
-
-var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
-    fmt.Println("Connected")
-}
-
-var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-    fmt.Printf("Connect lost: %v", err)
+//define a function for the default message handler
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
 }
 
 func main() {
-    var broker = "broker.emqx.io"
-    var port = 1883
-    opts := mqtt.NewClientOptions()
-    opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
-    opts.SetClientID("go_mqtt_client")
-    opts.SetUsername("emqx")
-    opts.SetPassword("public")
-    opts.SetDefaultPublishHandler(messagePubHandler)
-    opts.OnConnect = connectHandler
-    opts.OnConnectionLost = connectLostHandler
-    client := mqtt.NewClient(opts)
-    if token := client.Connect(); token.Wait() && token.Error() != nil {
-        panic(token.Error())
-  }
+	//create a ClientOptions struct setting the broker address, clientid, turn
+	//off trace output and set the default message handler
+	opts := MQTT.NewClientOptions().AddBroker("tcp://mqtt.eclipseprojects.io:1883")
+	opts.SetClientID("go-simple")
+	opts.SetDefaultPublishHandler(f)
+
+	//create and start a client using the above ClientOptions
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
+	//at a maximum qos of zero, wait for the receipt to confirm the subscription
+	if token := c.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
+	//from the server after sending each message
+	for i := 0; i < 5; i++ {
+		text := fmt.Sprintf("this is msg #%d!", i)
+		token := c.Publish("go-mqtt/sample", 0, false, text)
+		token.Wait()
+	}
+
+	time.Sleep(3 * time.Second)
+
+	//unsubscribe from /go-mqtt/sample
+	if token := c.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	c.Disconnect(250)
 }
